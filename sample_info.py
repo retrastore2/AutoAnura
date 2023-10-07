0b94218e4e7a9a78d5ff0c5ede71bdf220cbba49
retrastore2 <146480009+retrastore2@users.noreply.github.com>
2023-10-07 11:36:29 -0300

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 21529864
+API_HASH = '1a59a3c7c9012c7fc4da36de7f073cf7'
+BOT_TOKEN = '6651195992:AAFCAeNjc__N9bSwfDurIjuZq83yaj_PoDg'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,14 +11,14 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1111944484, 'admin123', 1111944484]
+CHANNELS = [--1001916290379, -1001744340044, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'Anurag'
+DATABASE_URI = "mongodb+srv://infofrantzy:FN@cluster0.zi5w2re.mongodb.net/"
+DATABASE_NAME = 'cluster03'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 
